e6a9e589e0ad48bc59f8208e8c455236eedf7f1b
Guedesmavi <mariavitoriaguedesferreira@gmail.com>
2026-05-19 15:57:49 -0300

    primeiro

---
+++ stack/src/navigation/StackNavegation.js
@@ -0,0 +1,18 @@
+import { createStackNavigator} from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+import DetailsScreen from "../screens/DetailsScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+    return (
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component = {HomeScreen} />
+          <Stack.Screen name="Details" component = {DetailsScreen} />
+          <Stack.Screen name="Profile" component = {ProfileScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export default StackNavigator;
